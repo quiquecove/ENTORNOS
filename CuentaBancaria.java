caaa2ab070502cb5d0e597f427c5d3ed6f7785aa
quiquecove <quiquehuertalopez@gmail.com>
2023-03-24 13:46:32 +0100

    Commit Rama2

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -5,6 +5,9 @@ class TestCuentaBancaria
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		
+		CuantaBancaria cuenta2= new CuentaBancaria("0123456789","Quique Huerta",1000,5);
+
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
