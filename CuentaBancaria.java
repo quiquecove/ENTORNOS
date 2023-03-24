9a775b474eb13c30b6ab7cacbc85b545946b2c6d
quiquecove <quiquehuertalopez@gmail.com>
2023-03-24 12:25:52 +0100

    primer commit

---
+++ CuentaBancaria.java
@@ -0,0 +1,13 @@
+class TestCuentaBancaria
+{
+	int TipoIntereses=5;
+
+	public static void main(String args[])
+	{
+		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		System.out.println("Saldo = " + cuenta.getSaldo());
+		cuenta.reintegro(50);
+		cuenta.ingreso(100);
+		System.out.println("Saldo: " + cuenta.getSaldo());
+	}
+}
